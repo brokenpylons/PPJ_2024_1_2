e141dde23c598a4cfb1409151ef9a72e7815720a
Žiga Leber <mail+github@zigaleber.com>
2024-02-20 18:07:57 +0100

    Add examples

--- test.sh
+++ test.sh
@@ -1,17 +1,29 @@
-onexit() {
-    rm -f pipe
-}
-trap onexit EXIT
-if [[ ! -p pipe ]]; then
-    mkfifo pipe
-fi
-
+#!/bin/bash
 pwd
-cat - <<<"INPUT" >pipe &
-output=$(./run.sh pipe)
-cat - <<<"$output"
 
-if [[ "$output" != "ANSWERINPUT" ]]
-then
-    exit 1
-fi
+newline() {
+    echo -e "\n"
+}
+
+declare -r path=$(dirname $(realpath "$0"))
+
+declare -i err=0
+
+for file in "$path"/examples/example{0..10}.txt
+do
+    echo "$file"
+    declare expected=$(cat "${file%.*}.response.txt")
+    declare output=$(./run.sh "$file" /dev/stdout)
+
+    if [[ "$output" == "$expected" ]]
+    then
+        echo -e "\n[OK]"
+    else
+        cat "$file"
+        wdiff <(cat - <<<"$output") <(cat - <<<"$expected")
+        echo -e "\n[FAIL]"
+        ((err++))
+    fi
+done
+
+exit $err
