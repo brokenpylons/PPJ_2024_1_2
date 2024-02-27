ba94b10dff958b0d0f35ac88439065faf17708f0
Žiga Leber <mail+github@zigaleber.com>
2024-02-27 18:52:49 +0100

    Fix test.sh

--- test.sh
+++ test.sh
@@ -9,21 +9,27 @@ declare -r path=$(dirname $(realpath "$0"))
 
 declare -i err=0
 
+declare -r tmp=$(mktemp)
+
 for file in "$path"/examples/example{0..14}.txt
 do
     echo "$file"
     declare expected=$(cat "${file%.*}.response.txt")
-    declare output=$(./run.sh "$file" /dev/stdout)
+
+    :> "$tmp"
+    ./run.sh "$file" "$tmp"
+    declare output=$(cat "$tmp")
 
     if [[ "$output" == "$expected" ]]
     then
         echo -e "\n[OK]\n"
     else
         cat "$file"
-        wdiff <(cat - <<<"$output") <(cat - <<<"$expected")
+        wdiff <(cat -A - <<<"$output") <(cat -A - <<<"$expected")
         echo -e "\n[FAIL]\n"
         ((err++))
     fi
 done
+rm "$tmp"
 
 exit $err
